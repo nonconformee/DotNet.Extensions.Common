f018f2ac04d6b5394dacb2bfd94a1b6748032257
nonconformee <69051148+AndreasRoten@users.noreply.github.com>
2025-11-21 15:12:46 +0100

    Exceptions

---
+++ src/Exceptions/ExceptionDetails.cs
@@ -0,0 +1,91 @@
+﻿
+namespace nonconformee.DotNet.Extensions.Exceptions;
+
+/// <summary>
+/// Specifies the details to include when representing an exception as a string (e.g. for error messages or logging).
+/// </summary>
+[Flags]
+public enum ExceptionDetails
+{
+    // --------------
+    // Common details
+    // --------------
+
+    /// <summary>
+    /// No exception details.
+    /// </summary>
+    None = 0x00000000,
+
+    /// <summary>
+    /// All type information.
+    /// </summary>
+    Type = TypeName | TypeNamespace | TypeAssembly,
+
+    /// <summary>
+    /// All exception details.
+    /// </summary>
+    All = 0x0FFFFFFF,
+
+    // -------------------------
+    // Exception type properties
+    // -------------------------
+
+    /// <summary>
+    /// The <see cref="Exception.StackTrace"/> property."/>
+    /// </summary>
+    StackTrace = 0x00000001,
+
+    /// <summary>
+    /// The <see cref="Exception.Source"/> property."/>
+    /// </summary>
+    Source = 0x00000002,
+
+    /// <summary>
+    /// The <see cref="Exception.Message"/> property."/>
+    /// </summary>
+    Message = 0x00000004,
+
+    /// <summary>
+    /// The <see cref="Exception.HResult"/> property."/>
+    /// </summary>
+    HResult = 0x00000008,
+
+    /// <summary>
+    /// The <see cref="Exception.Data"/> property."/>
+    /// </summary>
+    Data = 0x00000010,
+
+    /// <summary>
+    /// The <see cref="Exception.TargetSite"/> property."/>
+    /// </summary>
+    TargetSite = 0x00000020,
+
+    /// <summary>
+    /// The <see cref="Exception.HelpLink"/> property."/>
+    /// </summary>
+    HelpLink = 0x00000040,
+
+    // -------------------------
+    // Exception type reflection
+    // -------------------------
+
+    /// <summary>
+    /// The type name of the exception (without namespace, without Assembly).
+    /// </summary>
+    TypeName = 0x00000100,
+
+    /// <summary>
+    /// The namespace in which the exception type is defined.
+    /// </summary>
+    TypeNamespace = 0x00000200,
+
+    /// <summary>
+    /// The assembly in which the exception type is defined.
+    /// </summary>
+    TypeAssembly = 0x00000400,
+
+    /// <summary>
+    /// All other properties of the exception type (excluding those already listed in this enumeration), especially those specified by the actual/concrete exception type.
+    /// </summary>
+    Properties = 0x00000800,
+}
--- src/Exceptions/ExceptionExtensions.cs
+++ src/Exceptions/ExceptionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿
+using nonconformee.DotNet.Extensions.Collections;
+using System.Collections;
 using System.Globalization;
 using System.Text;
 
@@ -14,14 +16,13 @@ public static class ExceptionExtensions
     /// </summary>
     /// <remarks>
     /// This method is intended to provide human-readyble text describing exceptions (e.g. for debugging or logging).
-    /// If separators are added (<paramref name="addSeparator"/> is <see langword="true"/>) the string <c>--- inner exception ---</c> is added between the messages of each exception.
     /// The output starts from <paramref name="exception"/> to the innermost exception (through <see cref="Exception.InnerException"/>).
     /// </remarks>
     /// <param name="exception">The exception. Cannot be <see langword="null"/>.</param>
-    /// <param name="addSeparator">Specifies whether a separator shhall be inserted between exception messages. Default value is <see langword="true"/>.</param>
+    /// <param name="separator">The optional separator to be inserted between exception messages. Default value is <c>--- inner exception---</c>. Use <see langword="null"/> to not insert separators.</param>
     /// <returns>The full message.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
-    public static string GetFullMessage(this Exception exception, bool addSeparator = true)
+    public static string GetFullMessage(this Exception exception, string? separator = "--- inner exception ---")
     {
         if (exception is null) throw new ArgumentNullException(nameof(exception));
 
@@ -29,9 +30,9 @@ public static class ExceptionExtensions
 
         exception.GetAllExceptions().ForEach(e =>
         {
-            if (addSeparator && sb.Length > 0)
+            if ((separator is not null) && (sb.Length > 0))
             {
-                sb.AppendLine("--- inner exception ---");
+                sb.AppendLine(separator);
             }
 
             sb.AppendLine(e.Message);
@@ -46,15 +47,14 @@ public static class ExceptionExtensions
     /// </summary>
     /// <remarks>
     /// This method is intended to provide human-readyble text describing exceptions (e.g. for debugging or logging).
-    /// For exceptions for which no stack trace is available, <c>(no stack trace available)</c> is inserted.
-    /// If separators are added (<paramref name="addSeparator"/> is <see langword="true"/>) the string <c>--- inner exception ---</c> is added between the stack traces of each exception.
     /// The output starts from <paramref name="exception"/> to the innermost exception (through <see cref="Exception.InnerException"/>).
     /// </remarks>
     /// <param name="exception">The exception. Cannot be <see langword="null"/>.</param>
-    /// <param name="addSeparator">Specifies whether a separator shhall be inserted between exception messages. Default value is <see langword="true"/>.</param>
+    /// <param name="separator">The optional separator to be inserted between exception stack traces. Default value is <c>--- inner exception---</c>. Use <see langword="null"/> to not insert separators.</param>
+    /// <param name="noStackTrace">The optional text inserted as a stack trace entry if an exception does not provide its own stack trace. Default value is <c>(no stack trace available)</c>. Use <see langword="null"/> to omit exceptions without stack traces.</param>
     /// <returns>The full stack trace.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
-    public static string GetFullStackTrace(this Exception exception, bool addSeparator = true)
+    public static string GetFullStackTrace(this Exception exception, string? separator = "--- inner exception ---", string? noStackTrace = "(no stack trace available)")
     {
         if (exception is null) throw new ArgumentNullException(nameof(exception));
 
@@ -62,16 +62,19 @@ public static class ExceptionExtensions
 
         exception.GetAllExceptions().ForEach(e =>
         {
-            if(addSeparator && sb.Length > 0)
+            if ((separator is not null) && (sb.Length > 0))
             {
-                sb.AppendLine("--- inner exception ---");
+                sb.AppendLine(separator);
             }
 
             var stackTrace = e.StackTrace;
 
             if(string.IsNullOrEmpty(stackTrace))
             {
-                sb.AppendLine("(no stack trace available)");
+                if(noStackTrace is not null)
+                {
+                    sb.AppendLine(noStackTrace);
+                }
             }
             else
             {
@@ -83,18 +86,20 @@ public static class ExceptionExtensions
     }
 
     /// <summary>
-    /// Retrieves the full details of an exception, including from all inner exceptions.
+    /// Creates a formatted string containing detailed information about the exception, including selected properties and optionally all inner exceptions.
     /// </summary>
     /// <remarks>
     /// This method is intended to provide human-readyble text describing exceptions (e.g. for debugging or logging).
-    /// If separators are added (<paramref name="addSeparator"/> is <see langword="true"/>) the string <c>--- inner exception ---</c> is added between the messages of each exception.
     /// The output starts from <paramref name="exception"/> to the innermost exception (through <see cref="Exception.InnerException"/>).
     /// </remarks>
     /// <param name="exception">The exception. Cannot be <see langword="null"/>.</param>
-    /// <param name="addSeparator">Specifies whether a separator shhall be inserted between exception messages. Default value is <see langword="true"/>.</param>
-    /// <returns>The full details.</returns>
+    /// <param name="details">A bitwise combination of values that specifies which details to include in the output. The default is <see cref="ExceptionDetails.All"/>.</param>
+    /// <param name="recursive"><see langword="true"/> to include details from all inner exceptions recursively, <see langword="false"/> otherwise. The default is <see langword="true"/>.</param>
+    /// <param name="separator">The optional separator to be inserted between exception details. Default value is <c>--- inner exception---</c>. Use <see langword="null"/> to not insert separators.</param>
+    /// <param name="emptyOrNullValue">The optional text used for values which are <see langword="null"/> or an empty string. Default value is <c>(nothing)</c>. Use <see langword="null"/> to omit empty or null values in the output.</param>
+    /// <returns>The exception details.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
-    public static string GetFullDetails(this Exception exception, bool addSeparator = true)
+    public static string GetDetails(this Exception exception, ExceptionDetails details = ExceptionDetails.All, bool recursive = true, string? separator = "--- inner exception ---", string? emptyOrNullValue = "(nothing)")
     {
         if (exception is null) throw new ArgumentNullException(nameof(exception));
 
@@ -102,25 +107,167 @@ public static class ExceptionExtensions
 
         exception.GetAllExceptions().ForEach(e =>
         {
-            if (addSeparator && sb.Length > 0)
+            if ((separator is not null) && (sb.Length > 0))
             {
-                sb.AppendLine("--- inner exception ---");
+                sb.AppendLine(separator);
             }
 
-            var type = e.GetType().Name;
-            var message = e.Message;
-            var targetSite = e.TargetSite?.Name ?? "(unknown)";
-            var source = e.Source ?? "(unknown)";
-            var hresult = e.HResult.ToString(CultureInfo.InvariantCulture);
-            var stackTrace = e.StackTrace ?? "(no stack trace available)";
+            var type = e.GetType();
 
-            sb.AppendLine($"Type: {type}");
-            sb.AppendLine($"Message: {message}");
-            sb.AppendLine($"TargetSite: {targetSite}");
-            sb.AppendLine($"Source: {source}");
-            sb.AppendLine($"HResult: {hresult}");
-            sb.AppendLine($"StackTrace:");
-            sb.AppendLine($"{stackTrace}");
+            if (details.HasFlag(ExceptionDetails.TypeName))
+            {
+                var typeName = type.Name;
+                if (!string.IsNullOrWhiteSpace(typeName))
+                {
+                    sb.AppendLine($"Type: {typeName}");
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"Type: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.TypeNamespace))
+            {
+                var typeNamespace = type.Namespace;
+                if (!string.IsNullOrWhiteSpace(typeNamespace))
+                {
+                    sb.AppendLine($"Namespace: {typeNamespace}");
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"Namespace: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.TypeAssembly))
+            {
+                var typeAssembly = type.Assembly.FullName;
+                if(!string.IsNullOrWhiteSpace(typeAssembly))
+                {
+                    sb.AppendLine($"Assembly: {typeAssembly}");
+                }
+                else if(emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"Assembly: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.Message))
+            {
+                var message = e.Message;
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    sb.AppendLine($"Message: {message}");
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"Message: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.HResult))
+            {
+                sb.AppendLine($"HResult: {e.HResult.ToString(CultureInfo.InvariantCulture)}");
+            }
+
+            if (details.HasFlag(ExceptionDetails.Source))
+            {
+                var source = e.Source;
+                if (!string.IsNullOrWhiteSpace(source))
+                {
+                    sb.AppendLine($"Source: {source}");
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"Source: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.TargetSite))
+            {
+                var targetSite = e.TargetSite?.Name;
+                if (!string.IsNullOrWhiteSpace(targetSite))
+                {
+                    sb.AppendLine($"TargetSite: {targetSite}");
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"TargetSite: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.HelpLink))
+            {
+                var helpLink = e.HelpLink;
+                if (!string.IsNullOrWhiteSpace(helpLink))
+                {
+                    sb.AppendLine($"HelpLink: {helpLink}");
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"HelpLink: {emptyOrNullValue}");
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.Data) && e.Data is not null)
+            {
+                foreach(DictionaryEntry kv in e.Data)
+                {
+                    var key = kv.Key?.ToString();
+                    var value = kv.Value?.ToString();
+
+                    if(key is null)
+                    {
+                        continue;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        sb.AppendLine($"Data: {key}: {value}");
+                    }
+                    else if (emptyOrNullValue is not null)
+                    {
+                        sb.AppendLine($"Data: {key}: {emptyOrNullValue}");
+                    }
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.Properties))
+            {
+                var properties = type
+                    .GetProperties()
+                    .Where(x => x.DeclaringType != typeof(Exception));
+
+                foreach (var property in properties)
+                {
+                    var name = property.Name;
+                    var value = property.GetValue(e)?.ToString();
+
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        sb.AppendLine($"{name}: {value}");
+                    }
+                    else if (emptyOrNullValue is not null)
+                    {
+                        sb.AppendLine($"{name}: {emptyOrNullValue}");
+                    }
+                }
+            }
+
+            if (details.HasFlag(ExceptionDetails.StackTrace))
+            {
+                var stackTrace = e.StackTrace;
+                if (!string.IsNullOrWhiteSpace(stackTrace))
+                {
+                    sb.AppendLine("StackTrace:");
+                    sb.AppendLine(stackTrace);
+                }
+                else if (emptyOrNullValue is not null)
+                {
+                    sb.AppendLine($"StackTrace: {emptyOrNullValue}");
+                }
+            }
         });
 
         return sb.ToString().Trim();
